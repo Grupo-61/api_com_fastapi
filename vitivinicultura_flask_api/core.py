f182b630cea8762653eb6e018757e1b409b484da
ordepzero <ulissesph@gmail.com>
2025-04-13 17:40:30 -0300

    Ajuste consulta com parâmetros

--- vitivinicultura_flask_api/core.py
+++ vitivinicultura_flask_api/core.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from utils import verify_option_value
  
 
 links = [
@@ -30,6 +31,7 @@ def list_buttons_values(soup, class_name="btn_sopt"):
     buttons = soup.find_all("button", class_=class_name)
     for button in buttons:
         print(button.text)
+
 
 def crawler_wine_prodution_data(url, option):
     """
@@ -84,7 +86,32 @@ def crawler_footer_table(soup,header_list):
 
     return tfoot_data
 
-def crawler_options_table(soup):
+def extract_options_values_table(url):
+    '''
+    docstring:
+    Esta função extrai os valores dos botões de opção da página analisada.
+    Parameters:
+    url (str): URL da página a ser analisada.
+    Returns:
+    list: lista de dicionários contendo os valores e textos dos botões de opção.
+    '''
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    table = soup.find('table', class_='tb_base tb_header no_print')
+    if table:   
+        buttons = table.find_all('button', class_='btn_sopt')
+        buttons_values = [ {"value": butt.get('value'), 
+                            "text": butt.text.strip(), 
+                            "active": butt.has_attr('id')}
+            for butt in buttons  ]
+        
+        any_active_bu = any(item.get('id') == True for item in buttons_values)
+        
+        # Se nenhum item ativo, atribuir True ao primeiro item
+        if not any_active_bu and buttons_values:
+            buttons_values[0]['active'] = True
+
+    return buttons_values
 
 def crawler_rows_table(wine_table, header_list): 
     row_item_dict = {}
@@ -112,33 +139,51 @@ def crawler_rows_table(wine_table, header_list):
                 row_list.append(row_dict)
     return row_list
 
-def crawler_wine_table(url, table_class=None, button_opt=None, input_value=None):
+def crawler_wine_table(url, table_class=None, button_opt=None, input_year_value=None):
 
     response = requests.get(url)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "html.parser")
+        
+        # Verifica se a URL contém o parâmetro de opção válido
+        if button_opt:
+            option_values = extract_options_values_table(url)
+            is_option_value_valid = verify_option_value(option_values, "value", button_opt)
+            if is_option_value_valid:
+                url = url+f"&subopcao={button_opt}"
+                response = requests.get(url)
+                soup = BeautifulSoup(response.text, "html.parser")
+            else:
+                return {"Parâmetro inválido": f"O parâmetro de opção '{button_opt}' não é válido."}
+        
+        # Verifica se a URL contém o parâmetro de opção válido
+        if input_year_value:
+            soup = extract_interval_and_submit(url, input_year_value, button_opt)
 
         wine_table = None
+
 
         if table_class:
             wine_tables = soup.find_all("table", class_=table_class)
         else:
             wine_tables = soup.find_all("table")
         
+        
         tables = []
         if wine_tables:
             header_list = []
             row_list = []
 
-            for wine_table in wine_tables:  
+            for wine_table in wine_tables: 
+
+                
                 current_table = {}    
                 current_table["url"] = url
 
                 # Extrair o título da tabela
                 title_text = crawler_title_table(soup)
                 current_table["title"] = title_text
-
                 # Extrair o ano da tabela
                 year_table = extract_year(title_text)
                 current_table["year"] = year_table
@@ -146,7 +191,7 @@ def crawler_wine_table(url, table_class=None, button_opt=None, input_value=None)
                 # Encontrar o cabeçalho da tabela
                 header_list = crawler_header_table(wine_table)
                 current_table["header"] = header_list
-
+                
                 # Encontrar todas as linhas <tr> dentro da tabela                
                 row_list = crawler_rows_table(wine_table, header_list)
                 current_table["row_list"] = row_list
@@ -154,11 +199,65 @@ def crawler_wine_table(url, table_class=None, button_opt=None, input_value=None)
                 # Encontrar o rodapé da tabela         
                 tfoot_data = crawler_footer_table(soup,header_list)
                 current_table["footer"] = tfoot_data
-            
-            tables = tables.append(current_table)
-    print(current_table.get("title"),current_table.get("year"))
+                
+            tables.append(current_table)
+    # print(current_table.get("title"),current_table.get("year"))
     return tables
 
-if __name__ == "__main__":
-    crawler_wine_table(links[3],"tb_base tb_dados")
 
+def extract_interval_and_submit(url, year_to_submit, option=None):
+    
+    response = requests.get(url)
+    response.encoding = 'utf-8' 
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Encontrar o texto do intervalo de anos
+    label = soup.find('label', class_='lbl_pesq')
+    if label:
+        interval_text = label.text.strip()  # Extrair o texto do label
+        print("Texto do intervalo:", interval_text)
+
+        # Extrair os limites do intervalo de anos
+        years = interval_text.split("[")[1].split("]")[0].split("-")  # [1970-2024]
+        year_min, year_max = int(years[0]), int(years[1])
+
+        print(f"Intervalo de anos disponível: {year_min}-{year_max}")
+
+        # Verificar se o ano está dentro do intervalo que foi extraído da página
+        if year_min <= year_to_submit <= year_max:
+            print(f"O ano {year_to_submit} está dentro do intervalo permitido ({year_min}-{year_max}).")
+
+            # Colocando os dados para submter o forms
+            form_data = {
+                "ano": year_to_submit
+            }
+
+            if option:
+                form_data["subopcao"] = option
+
+            # Fazer a submissão do formulário
+            submit_response = requests.post(url, data=form_data)
+            if submit_response.status_code == 200:
+                print("Formulário submetido com sucesso!")
+                return submit_response
+            else:
+                print("Erro ao submeter o formulário:", submit_response.status_code)
+                return None
+        else:
+            message = f"O ano {year_to_submit} está fora do intervalo permitido ({year_min}-{year_max})."
+            print(message)
+            return None # {"message": f"{message}"}
+    else:
+        print("Intervalo de anos não encontrado no HTML.")
+    return None
+
+if __name__ == "__main__":
+    #(url, table_class=None, button_opt=None, input_year_value=None)
+    r = crawler_wine_table(links[1],"tb_base tb_dados","subopt_02", 2020)
+    print(r)
+    # Exemplo de uso
+    # url = links[3]
+    # ano_para_submeter = 1970  # Ano a ser submetido
+    # response = extract_interval_and_submit(url, ano_para_submeter)
+    # soup = BeautifulSoup(response.text, "html.parser")
